aa2fdf7aa703073c35f2f50668a25c3dc2a86aae
Owen TM <oturnermajor@gmail.com>
2016-03-02 22:05:15 +0000

    redis for heroku implemented

--- redisFunctions.js
+++ redisFunctions.js
@@ -1,7 +1,12 @@
 "use strict";
 
-var redis = require('redis');
-var client = redis.createClient();
+if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var client = require("redis").createClient();
+}
 
 function addPostToDB(postObject){
 	var postName = 'post' + postObject.date;
